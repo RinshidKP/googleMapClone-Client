3b0a2aad4a27c78b9183bc23a2ef828c40cfe842
RinshidKP <rinshid26@gmail.com>
2024-06-04 15:32:31 +0530

    updated urls

--- src/config/apiURL.js
+++ src/config/apiURL.js
@@ -1,5 +1,10 @@
 const apiURL = {
-    userAPI: 'http://localhost:3000/',
+    userAPI: 'https://googlemapclone-server.onrender.com/',
     adminAPI: 'http://localhost:3000/admin/',
 }
 export default apiURL
+// const apiURL = {
+//     userAPI: 'http://localhost:3000/',
+//     adminAPI: 'http://localhost:3000/admin/',
+// }
+// export default apiURL
